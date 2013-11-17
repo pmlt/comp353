37538b3efbc18c0907baf3ed115aa723f74781a0
Philippe Milot <philmilot@gmail.com>
2013-11-17 11:35:27 -0500

    Add routes, start rendering pages

---
+++ sems/actions/conference.php
@@ -0,0 +1,11 @@
+<?php
+
+function sems_conference($conf) {
+  // XXX check if conference exists.
+  return sems_notfound();
+}
+
+function sems_conference_search($conf) {
+  // XXX check if conference exists.
+  return sems_notfound();
+}
--- sems/routes.php
+++ sems/routes.php
@@ -1,11 +1,59 @@
 <?php
 
+include 'actions/conference.php';
+
+$HTTP_ROOT = '/comp353';
+
 function sems_routes() {
   //XXX must define routes here.
+  global $HTTP_ROOT;
   return array(
-    '/.*/' => 'sems_notfound');
+    //SEMS Administration routes
+    "|^{$HTTP_ROOT}/admin$|" => 'sems_admin',
+    "|^{$HTTP_ROOT}/admin/conferences$|" => 'sems_admin_conferences',
+    "|^{$HTTP_ROOT}/admin/conferences/create$|" => 'sems_admin_conferences_create',
+    "|^{$HTTP_ROOT}/admin/conferences/edit$|" => 'sems_admin_conferences_create',
+    
+    //User management routes
+    "|^{$HTTP_ROOT}/login$|" => 'sems_login',
+    "|^{$HTTP_ROOT}/logout$|" => 'sems_logout',
+    "|^{$HTTP_ROOT}/signup$|" => 'sems_signup',
+    "|^{$HTTP_ROOT}/confirm/(\w+)$|" => 'sems_confirm',
+    "|^{$HTTP_ROOT}/profile/(\w+)$|" => 'sems_profile',
+    "|^{$HTTP_ROOT}/profile/(\w+)/edit$|" => 'sems_profile_edit',
+    
+    //Conference routes
+    "|^{$HTTP_ROOT}/(\w+)$|" => 'sems_conference',
+    "|^{$HTTP_ROOT}/(\w+)/search$|" => 'sems_conference_search',
+    
+    //Event routes
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)$|" => 'sems_event',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/search$|" => 'sems_event_search',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/committee$|" => 'sems_event_committee',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/papers$|" => 'sems_event_papers',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/papers/(\w+)$|" => 'sems_event_paper',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/reviews$|" => 'sems_event_reviews',
+    "|^{$HTTP_ROOT}/(\w+)/(\w+)/review/(\w+)$|" => 'sems_event_review',
+    
+    //Home route
+    "|^{$HTTP_ROOT}/$|"    => 'sems_home',
+    
+    //Catch-all 404 route
+    '|.*|' => 'sems_notfound');
+}
+
+function sems_root() { global $HTTP_ROOT; return $HTTP_ROOT; }
+
+function sems_home_url() { return sems_root()."/"; }
+function sems_home() {
+  return sems_smarty(function($smarty) {
+    $body = $smarty->fetch('home.tpl');
+    return ok($body);
+  });
 }
 
 function sems_notfound() {
-  return notFound("SEMS: Page not found.");
+  return sems_smarty(function($smarty) {
+    return notFound($smarty->fetch('notfound.tpl'));
+  });
 }
