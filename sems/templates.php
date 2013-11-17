ea8058fdf0591ead7e3f08eef7501d4b7330c12a
Philippe Milot <philmilot@gmail.com>
2013-11-17 16:14:07 -0500

    Add convenience method to fetch content of single template

--- sems/templates.php
+++ sems/templates.php
@@ -7,5 +7,13 @@ function sems_smarty($f) {
   $smarty->setTemplateDir(SEMS_TEMPLATES_PATH);
   $smarty->setCompileDir(TMP_PATH.'/templates_c');
   $smarty->setCacheDir(TMP_PATH.'/cache');
+  $smarty->muteExpectedErrors();
   return call_user_func($f, $smarty);
+}
+
+function sems_smarty_fetch($path, $variables=array()) {
+  return sems_smarty(function($smarty) use(&$path, &$variables) {
+    $smarty->assign($variables);
+    return $smarty->fetch($path);
+  });
 }
