c5ab59947ad81dc95e1f4ec8f5b63a71ac3ff53e
Philippe Milot <philmilot@gmail.com>
2013-11-17 16:12:39 -0500

    Fix config and add real DB config

--- sems/config.php
+++ sems/config.php
@@ -1,14 +1,19 @@
 <?php
 
-// XXX define config here.
 $config = array(
-  'db' => array());
+  'db' => array(
+    'host' => 'localhost',
+    'db'   => 'comp353',
+    'user' => 'comp353',
+    'pwd'  => 'QyurjX7JX}c7:ZYnoT&PhKUFpV'
+  ));
 
 function sems_config($path) {
+  global $config;
   $parts = explode('.', $path);
   $c = $config;
   foreach ($parts as $part) {
-    $c &= $c[$part];
+    $c = $c[$part];
   }
   return $c;
 }
