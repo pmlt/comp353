afe3d8d3cdd9d546826c4a6d6f997d51c297b329
Philippe Milot <philmilot@gmail.com>
2013-12-01 11:39:37 -0500

    Add ON DELETE and ON UPDATE policies to all FK

--- scripts/sql/schema-1000.sql
+++ scripts/sql/schema-1000.sql
@@ -27,7 +27,7 @@ CREATE TABLE `Conference` (
   `description` TEXT NOT NULL,
   `type` ENUM('J','C') NOT NULL,
   `chair_id` int(11) NOT NULL,
-  CONSTRAINT `fk_Conference_chair_id` FOREIGN KEY `chair_id` (`chair_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_Conference_chair_id` FOREIGN KEY `chair_id` (`chair_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `ConferenceTopic`;
@@ -35,8 +35,8 @@ CREATE TABLE `ConferenceTopic` (
   `conference_id` INT(11) NOT NULL,
   `topic_id` INT(11) NOT NULL,
   PRIMARY KEY (`conference_id`,`topic_id`),
-  CONSTRAINT `fk_ConferenceTopic_conference_id` FOREIGN KEY `conference_id` (`conference_id`) REFERENCES `Conference` (`conference_id`),
-  CONSTRAINT `fk_ConferenceTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`)
+  CONSTRAINT `fk_ConferenceTopic_conference_id` FOREIGN KEY `conference_id` (`conference_id`) REFERENCES `Conference` (`conference_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ConferenceTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `Event`;
@@ -58,8 +58,8 @@ CREATE TABLE `Event` (
   `end_date` DATETIME NOT NULL,
   `chair_id` INT(11) NOT NULL,
   UNIQUE KEY `unique_title` (`conference_id`,`title`),
-  CONSTRAINT `fk_Event_conference_id` FOREIGN KEY `conference_id` (`conference_id`) REFERENCES `Conference` (`conference_id`),
-  CONSTRAINT `fk_Event_chair_id` FOREIGN KEY `chair_id` (`chair_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_Event_conference_id` FOREIGN KEY `conference_id` (`conference_id`) REFERENCES `Conference` (`conference_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_Event_chair_id` FOREIGN KEY `chair_id` (`chair_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `EventTopic`;
@@ -67,8 +67,8 @@ CREATE TABLE `EventTopic` (
   `event_id` INT(11) NOT NULL,
   `topic_id` INT(11) NOT NULL,
   PRIMARY KEY (`event_id`,`topic_id`),
-  CONSTRAINT `fk_EventTopic_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`),
-  CONSTRAINT `fk_EventTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`)
+  CONSTRAINT `fk_EventTopic_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_EventTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `CommitteeMembership`;
@@ -76,8 +76,8 @@ CREATE TABLE `CommitteeMembership` (
   `event_id` INT(11) NOT NULL,
   `user_id` INT(11) NOT NULL,
   PRIMARY KEY (`event_id`,`user_id`),
-  CONSTRAINT `fk_CommitteeMembership_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`),
-  CONSTRAINT `fk_CommitteeMembership_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_CommitteeMembership_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_CommitteeMembership_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `Paper`;
@@ -91,7 +91,7 @@ CREATE TABLE `Paper` (
   `decision` ENUM('pending','full','short','poster','workshop','position','demo','rejected') NOT NULL,
   `decision_date` DATETIME DEFAULT NULL,
   `publish_date` DATETIME DEFAULT NULL,
-  CONSTRAINT `fk_Paper_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`)
+  CONSTRAINT `fk_Paper_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `PaperAuthor`;
@@ -99,8 +99,8 @@ CREATE TABLE `PaperAuthor` (
   `paper_id` INT(11) NOT NULL,
   `user_id` INT(11) NOT NULL,
   PRIMARY KEY (`paper_id`,`user_id`),
-  CONSTRAINT `fk_PaperAuthor_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`),
-  CONSTRAINT `fk_PaperAuthor_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_PaperAuthor_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_PaperAuthor_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `PaperTopic`;
@@ -108,8 +108,8 @@ CREATE TABLE `PaperTopic` (
   `paper_id` INT(11) NOT NULL,
   `topic_id` INT(11) NOT NULL,
   PRIMARY KEY (`paper_id`,`topic_id`),
-  CONSTRAINT `fk_PaperTopic_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`),
-  CONSTRAINT `fk_PaperTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`)
+  CONSTRAINT `fk_PaperTopic_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_PaperTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `PaperVersion`;
@@ -117,7 +117,7 @@ CREATE TABLE `PaperVersion` (
   `paper_id` INT(11) NOT NULL,
   `revision_date` DATETIME NOT NULL,
   PRIMARY KEY (`paper_id`,`revision_date`),
-  CONSTRAINT `fk_PaperVersion_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`)
+  CONSTRAINT `fk_PaperVersion_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `PaperReview`;
@@ -133,9 +133,9 @@ CREATE TABLE `PaperReview` (
   `chair_comments` TEXT NOT NULL,
   `external_reviewer_id` INT(11) DEFAULT NULL,
   PRIMARY KEY (`paper_id`,`reviewer_id`),
-  CONSTRAINT `fk_PaperReview_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`),
-  CONSTRAINT `fk_PaperReview_reviewer_id` FOREIGN KEY `reviewer_id` (`reviewer_id`) REFERENCES `User` (`user_id`),
-  CONSTRAINT `fk_PaperReview_external_reviewer_id` FOREIGN KEY `external_reviewer_id` (`external_reviewer_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_PaperReview_paper_id` FOREIGN KEY `paper_id` (`paper_id`) REFERENCES `Paper` (`paper_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_PaperReview_reviewer_id` FOREIGN KEY `reviewer_id` (`reviewer_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_PaperReview_external_reviewer_id` FOREIGN KEY `external_reviewer_id` (`external_reviewer_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `Message`;
@@ -148,8 +148,8 @@ CREATE TABLE `Message` (
   `title` TEXT NOT NULL,
   `excerpt` TEXT NOT NULL,
   `body` TEXT NOT NULL,
-  CONSTRAINT `fk_Message_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`),
-  CONSTRAINT `fk_Message_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_Message_event_id` FOREIGN KEY `event_id` (`event_id`) REFERENCES `Event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_Message_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `User`;
@@ -171,9 +171,9 @@ CREATE TABLE `User` (
   `password` TEXT NOT NULL,
   `email_sent_flag` BIT(1) NOT NULL DEFAULT b'0',
   `last_event_id` INT(11) DEFAULT NULL,
-  CONSTRAINT `fk_User_organization_id` FOREIGN KEY `organization_id` (`organization_id`) REFERENCES `Organization` (`organization_id`),
-  CONSTRAINT `fk_User_country_id` FOREIGN KEY `country_id` (`country_id`) REFERENCES `Country` (`country_id`),
-  CONSTRAINT `fk_User_last_event_id` FOREIGN KEY `last_event_id` (`last_event_id`) REFERENCES `Event` (`event_id`)
+  CONSTRAINT `fk_User_organization_id` FOREIGN KEY `organization_id` (`organization_id`) REFERENCES `Organization` (`organization_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_User_country_id` FOREIGN KEY `country_id` (`country_id`) REFERENCES `Country` (`country_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_User_last_event_id` FOREIGN KEY `last_event_id` (`last_event_id`) REFERENCES `Event` (`event_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `UserTopic`;
@@ -181,8 +181,8 @@ CREATE TABLE `UserTopic` (
   `user_id` INT(11) NOT NULL,
   `topic_id` INT(11) NOT NULL,
   PRIMARY KEY (`user_id`,`topic_id`),
-  CONSTRAINT `fk_UserTopic_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`),
-  CONSTRAINT `fk_UserTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`)
+  CONSTRAINT `fk_UserTopic_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_UserTopic_topic_id` FOREIGN KEY `topic_id` (`topic_id`) REFERENCES `Topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 DROP TABLE IF EXISTS `UserRole`;
@@ -190,7 +190,7 @@ CREATE TABLE `UserRole` (
   `user_id` INT(11) NOT NULL,
   `role` ENUM('admin','normal') NOT NULL,
   PRIMARY KEY (`user_id`,`role`),
-  CONSTRAINT `fk_UserRole_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`)
+  CONSTRAINT `fk_UserRole_user_id` FOREIGN KEY `user_id` (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET='utf8';
 
 SET FOREIGN_KEY_CHECKS=1;
