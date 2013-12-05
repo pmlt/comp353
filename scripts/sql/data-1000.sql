1b7df6bd44e82be403506afa2726e7b6549a1c47
Philippe Milot <philmilot@gmail.com>
2013-12-05 14:52:54 -0500

    New data to more easily demo and test

--- scripts/sql/data-1000.sql
+++ scripts/sql/data-1000.sql
@@ -2,17 +2,21 @@ INSERT INTO `Topic` VALUES
 (1, 'Compiler Techniques', 'Computer Science'),
 (2, 'Language Theory', 'Computer Science'),
 (3, 'Algorithm Design and Analysis', 'Computer Science'),
-(4, 'Scalability and Parallelism', 'Computer Science'),
-(5, 'Ornithology', 'Biology'),
-(6, 'Entomology', 'Biology'),
-(7, 'Microbiology', 'Biology'),
-(8, 'Astronomy', 'Physics'),
-(9, 'Atomic Theory', 'Physics'),
-(10, 'Newtonian Mechanics', 'Physics'),
-(11, 'Analysis', 'Mathematics'),
-(12, 'Number theory', 'Mathematics'),
-(13, 'Set Theory', 'Mathematics'),
-(14, 'Numerical Methods', 'Mathematics');
+(4, '3D Graphics', 'Computer Science'),
+(5, 'Cryptography', 'Computer Science'),
+(6, 'Concurrency and Parallelism', 'Computer Science'),
+(7, 'Scalable Systems', 'Computer Science'),
+(8, 'Operating Systems', 'Computer Science'),
+(9, 'Ornithology', 'Biology'),
+(10, 'Entomology', 'Biology'),
+(11, 'Microbiology', 'Biology'),
+(12, 'Astronomy', 'Physics'),
+(13, 'Atomic Theory', 'Physics'),
+(14, 'Newtonian Mechanics', 'Physics'),
+(15, 'Analysis', 'Mathematics'),
+(16, 'Number theory', 'Mathematics'),
+(17, 'Set Theory', 'Mathematics'),
+(18, 'Numerical Methods', 'Mathematics');
 
 INSERT INTO `Country` VALUES ('US', 'United States');
 INSERT INTO `Country` VALUES ('CA', 'Canada');
@@ -263,6 +267,7 @@ INSERT INTO `Organization` (name) VALUES('Google');
 INSERT INTO `Organization` (name) VALUES('Microsoft');
 INSERT INTO `Organization` (name) VALUES('Amazon');
 INSERT INTO `Organization` (name) VALUES('Apple');
+INSERT INTO `Organization` (name) VALUES('Turbulent inc.');
 
 INSERT INTO User VALUES
 (1, NOW(), 'dr', 'Admi', 'Ni', 'Strator', 'CA', 1, 'Computer Science', NULL, NULL, NULL, NULL, 'admin@encs.concordia.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
@@ -270,7 +275,16 @@ INSERT INTO User VALUES
 (3, NOW(), 'ms', 'Elaine', '', 'Benes', 'CA', 3, 'Publishing', NULL, NULL, NULL, NULL, 'elaine@seinfeld.com', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
 (4, NOW(), 'mr', 'George', '', 'Costanza', 'CA', 4, 'Publishing', NULL, NULL, NULL, NULL, 'costanza@seinfeld.com', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
 (5, NOW(), 'mr', 'Cosmo', '', 'Kramer', 'CA', 5, 'Modelling', NULL, NULL, NULL, NULL, 'kramer@seinfeld.com', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
-(6, NOW(), 'mr', 'Wayne', '', 'Newman', 'CA', 6, 'Janitor', NULL, NULL, NULL, NULL, 'newman@seinfeld.com', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL);
+(6, NOW(), 'mr', 'Wayne', '', 'Newman', 'CA', 6, 'Janitor', NULL, NULL, NULL, NULL, 'newman@seinfeld.com', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(7, NOW(), 'dr', 'Philippe', '', 'Milot', 'CA', 10, 'Programmer', NULL, NULL, NULL, NULL, 'pmilot@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(8, NOW(), 'ms', 'Kim', 'H.', 'Reeves', 'CA', 10, 'Programmer', NULL, NULL, NULL, NULL, 'kreeves@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(9, NOW(), 'dr', 'Benoit', 'B.', 'Beauséjour', 'CA', 10, 'Programmer', NULL, NULL, NULL, NULL, 'bbeausej@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(10, NOW(), 'mrs', 'Alice', '', 'Burger', 'CA', 10, 'Content manager', NULL, NULL, NULL, NULL, 'aburger@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(11, NOW(), 'mr', 'Félix', 'F.', 'Courtemanche', 'CA', 10, 'Programmer', NULL, NULL, NULL, NULL, 'fcourtemanche@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(12, NOW(), 'mrs', 'Claire', '', 'Buffer', 'CA', 10, 'Director of Production', NULL, NULL, NULL, NULL, 'cbuffet@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(13, NOW(), 'ms', 'Anne-Marie', 'M.', 'Champoux', 'CA', 10, 'Content manager', NULL, NULL, NULL, NULL, 'amchampoux@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(14, NOW(), 'dr', 'Marc', 'E.', 'Beaudet', 'CA', 10, 'President', NULL, NULL, NULL, NULL, 'mbeaudet@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL),
+(15, NOW(), 'mr', 'Marc', '', 'Cholette', 'CA', 10, 'Project Lead', NULL, NULL, NULL, NULL, 'mcholette@turbulent.ca', '05fe7461c607c33229772d402505601016a7d0ea', b'1', NULL);
 
 INSERT INTO UserRole VALUES
 (1, 'admin'),
@@ -279,57 +293,187 @@ INSERT INTO UserRole VALUES
 (3, 'normal'),
 (4, 'normal'),
 (5, 'normal'),
-(6, 'normal');
+(6, 'normal'),
+(7, 'normal'),
+(8, 'normal'),
+(9, 'normal'),
+(10, 'normal'),
+(11, 'normal'),
+(12, 'normal'),
+(13, 'normal'),
+(14, 'normal'),
+(15, 'normal');
 
 INSERT INTO `Conference` VALUES
-(1, 'Pure Mathematics Symposium', 'A symposium about the latest developments in the field of pure mathematics.', 'C', 2);
+(1, 'Pure Mathematics Symposium', 'A symposium about the latest developments in the field of pure mathematics.', 'C', 2),
+(2, 'Computer Science Journal', 'A daily journal which publishes articles about new development in computer science research.', 'J', 7);
 
 INSERT INTO `ConferenceTopic` VALUES
-(1, 11),
-(1, 12),
-(1, 13),
-(1, 14);
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(2, 7),
+(2, 8);
 
 INSERT INTO `Event` VALUES
 (1, 1, 'Discrete Mathematics', 'A meeting focusing on the area of discrete mathematics.', '2014-01-01 00:00:00', '2014-01-02 00:00:00', '2014-01-02 00:00:00', '2014-01-03 00:00:00', '2014-01-03 00:00:00', '2014-01-04 00:00:00', '2014-01-04 00:00:00', '2014-01-05 00:00:00', '2014-01-06 00:00:00', '2014-01-10 09:00:00', '2014-01-10 17:00:00', 2),
-(2, 1, 'Pure Mathematics', 'A meeting focusing on the area of pure mathematics.', '2014-01-01 00:00:00', '2014-01-02 00:00:00', '2014-01-02 00:00:00', '2014-01-03 00:00:00', '2014-01-03 00:00:00', '2014-01-04 00:00:00', '2014-01-04 00:00:00', '2014-01-05 00:00:00', '2014-01-06 00:00:00', '2014-01-11 09:00:00', '2014-01-11 17:00:00', 3);
+(2, 1, 'Pure Mathematics', 'A meeting focusing on the area of pure mathematics.', '2014-01-01 00:00:00', '2014-01-02 00:00:00', '2014-01-02 00:00:00', '2014-01-03 00:00:00', '2014-01-03 00:00:00', '2014-01-04 00:00:00', '2014-01-04 00:00:00', '2014-01-05 00:00:00', '2014-01-06 00:00:00', '2014-01-11 09:00:00', '2014-01-11 17:00:00', 3),
+(3, 2, 'Issue No. 1: Compiler Techniques', 'New developments in compiler techniques and algorithms.', 
+  '2013-11-30', '2013-12-01', '2013-12-01', '2013-12-02', '2013-12-02', '2013-12-03', '2013-12-03', '2013-12-04', '2013-12-05', '2013-12-06', '2013-12-06', 7),
+(4, 2, 'Issue No. 2: Language Theory', 'New developments in language research.', 
+  '2013-12-01', '2013-12-02', '2013-12-02', '2013-12-03', '2013-12-03', '2013-12-04', '2013-12-04', '2013-12-05', '2013-12-06', '2013-12-07', '2013-12-07', 7),
+(5, 2, 'Issue No. 3: Algorithms', 'The latest, greatest groundbreaking algorithms.',
+  '2013-12-02', '2013-12-03', '2013-12-03', '2013-12-04', '2013-12-04', '2013-12-05', '2013-12-05', '2013-12-06', '2013-12-07', '2013-12-08', '2013-12-08', 7),
+(6, 2, 'Issue No. 4: Graphics', 'A focus on 3D graphics processing techniques.',
+  '2013-12-03', '2013-12-04', '2013-12-04', '2013-12-05', '2013-12-05', '2013-12-06', '2013-12-06', '2013-12-07', '2013-12-08', '2013-12-09', '2013-12-09', 7),
+(7, 2, 'Issue No. 5: Cryptography', 'A focus on cryptographic techniques and algorithms.',
+  '2013-12-04', '2013-12-05', '2013-12-05', '2013-12-06', '2013-12-06', '2013-12-07', '2013-12-07', '2013-12-08', '2013-12-09', '2013-12-10', '2013-12-10', 7),
+(8, 2, 'Issue No. 6: Parallelism and Concurrency', 'A focus on programming in multithreaded environments.',
+  '2013-12-05', '2013-12-06', '2013-12-06', '2013-12-07', '2013-12-07', '2013-12-08', '2013-12-08', '2013-12-09', '2013-12-10', '2013-12-11', '2013-12-11', 7),
+(9, 2, 'Issue No. 7: Scalable Systems', 'A focus on real-world examples of systems built for high scalability.',
+  '2013-12-06', '2013-12-07', '2013-12-07', '2013-12-08', '2013-12-08', '2013-12-09', '2013-12-09', '2013-12-10', '2013-12-11', '2013-12-12', '2013-12-12', 7),
+(10, 2, 'Issue No. 8: Microkernels', 'A focus on the latest developments in the world of microkernel operating systems.', 
+  '2013-12-07', '2013-12-08', '2013-12-08', '2013-12-09', '2013-12-09', '2013-12-10', '2013-12-10', '2013-12-11', '2013-12-12', '2013-12-13', '2013-12-13', 7);
 
 INSERT INTO `EventTopic` VALUES
-(2, 11),
-(2, 14), 
-(1, 12),
-(1, 13);
+(1,11),
+(2,12),
+(3,1),
+(3,3),
+(4,2),
+(5,3),
+(6,4),
+(6,7),
+(7,5),
+(7,3),
+(8,6),
+(8,3),
+(9,7),
+(10,8),
+(10,7);
 
 INSERT INTO CommitteeMembership VALUES
-(1, 4),
-(2, 5);
+(1,4),
+(2,5),
+(3,8),
+(3,9),
+(4,10),
+(4,11),
+(5,12),
+(5,13),
+(6,14),
+(6,15),
+(7,9),
+(7,10),
+(8,11),
+(8,12),
+(9,13),
+(9,14);
 
 INSERT INTO `Paper` VALUES
 (1, 'On Set Theory', 'Abstract of On Set Theory', 'set theory,math', 5, 1, 'pending', NULL, NULL),
-(2, 'Why Pure Mathematics is the most useful science', 'Abstract of Why Pure...', 'philosophy,math', 6, 2, 'rejected', '2014-01-05 14:00:00', NULL);
+(2, 'Why Pure Mathematics is the most useful science', 'Abstract of Why Pure...', 'philosophy,math', 6, 2, 'rejected', '2014-01-05 14:00:00', NULL),
+(3, 'A new take on LARL Parsers', 'Discuss a new spin on the old LARL parser algorithm.', 'lalr,parser,compiler', 12, 3, 'full', '2013-12-06 10:00:00', NULL),
+(4, 'Interpreted programs: Who cares anymore?', 'With the latest advances in JIT compilation, are interpreters becoming old-fashioned?', 'interpreted,perl,php', 13, 3, 'short', '2013-12-06 11:00:00', NULL),
+(5, 'The rise of functional programming', 'FP is becoming more and more common. Are we on the brink of an FP revolution?', 'functional,fp,haskell,scala', 14, 4, 'full', NULL, NULL),
+(6, 'FORTRAN: The warrior that just will not go away', '40 years after its creation, FORTRAN remains a popular language among computer scientists. Why?', 'fortran,language', 15, 4, 'rejected', NULL, NULL),
+(7, 'Aspect-oriented programming: OOP on steroids', 'There is no question that OOP remains the most popular paradigm today. But it is about to get even better soon.', 'aspect,object,trait,mixin', 8, 4, 'workshop', NULL, NULL),
+(8, 'Parallel sorting', 'Can we finally replace quicksort using parallelism?', 'sorting,parallelism,quicksort', 9, 5, 'pending', NULL, NULL),
+(9, 'Graph traversal catalogue', 'A catalogue of popular graph traversal algorithms.', 'graph,traversal,tree', 10, 5, 'pending', NULL, NULL),
+(10, 'A second look at GPU processing', 'An updated overview of current GPU architectures.', 'gpu,architecture,graphics', 11, 6, 'pending', NULL, NULL),
+(11, 'Next-generation shaders', 'A look at the next generation of shader processing techniques.', 'shaders,graphics,opengl', 12, 6, 'pending', NULL, NULL),
+(12, 'Just what is RSA?', 'A summary of the main design of the RSA encryption algorithm', 'rsa,encryption', 13, 7, 'pending', NULL, NULL),
+(13, 'The National Security Agency has ruined cryptography forever', 'We dive in details into the consequences of NSA meddling in the field of cryptography.', 'nsa,politics', 14, 7, 'pending', NULL, NULL);
 
 INSERT INTO `PaperAuthor` VALUES
 (1, 5),
 (1, 6),
 (2, 6), 
-(2, 3);
+(2, 3),
+(3, 12),
+(4, 13),
+(5, 14),
+(5, 8),
+(6, 15),
+(7, 8),
+(8, 9),
+(9, 10),
+(9, 11),
+(9, 12),
+(10, 11),
+(11, 12),
+(12, 13),
+(13, 14);
 
 INSERT INTO `PaperTopic` VALUES
 (1, 13),
-(2, 11);
+(2, 11),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 2),
+(7, 2),
+(8, 3),
+(9, 3),
+(10, 4),
+(11, 4),
+(12, 5),
+(13, 5);
 
 INSERT INTO `PaperVersion` VALUES
 (1, '2014-01-02 09:00:00'),
 (1, '2014-01-02 10:00:00'),
-(2, '2014-01-02 14:00:00');
+(2, '2014-01-01 14:00:00'),
+(3, '2013-12-01 14:00:00'),
+(4, '2013-12-02 14:00:00'),
+(5, '2013-12-02 14:00:00'),
+(6, '2013-12-02 14:00:00'),
+(6, '2013-12-02 15:00:00'),
+(6, '2013-12-02 16:00:00'),
+(7, '2013-12-02 14:00:00'),
+(8, '2013-12-03 14:00:00'),
+(9, '2013-12-03 14:00:00'),
+(10, '2013-12-04 14:00:00'),
+(11, '2013-12-04 14:00:00'),
+(12, '2013-12-05 14:00:00'),
+(13, '2013-12-05 14:00:00');
 
 INSERT INTO `PaperBid` VALUES
 (4, 1),
-(5, 2);
+(5, 2),
+(8, 3),
+(9, 4),
+(9, 3),
+(8, 4),
+(10, 5),
+(10, 6),
+(10, 7),
+(11, 6),
+(11, 7),
+(12, 8),
+(13, 9),
+(14, 10),
+(14, 11),
+(15, 11);
 
 INSERT INTO `PaperReview` VALUES
 (1, 1, 4, NULL, NULL, NULL, '', '', '', '', NULL),
-(2, 2, 5, 3, 4, 'bad', 'Well-written.', 'This isn\'t really about math per se.', 'Are you sure this belongs to the symposium?', 'This seems more philosophical in nature than purely scientific.', NULL);
+(2, 2, 5, 3, 4, 'bad', 'Well-written.', 'This isn\'t really about math per se.', 'Are you sure this belongs to the symposium?', 'This seems more philosophical in nature than purely scientific.', NULL),
+(3, 3, 8, 8, 9, 'good', 'Original point of view.', 'Author really digs deep on the subject. Well-documented process.', 'Congratulations, very well written!', 'Strongly recommend inclusion in this issue.', NULL),
+(4, 4, 9, 6, 10, 'mediocre', 'Wel-written.', 'A well-written article about a boring subject.', 'You write well, but the subject is not very interesting.', 'Include in issue only if we don\'t have enough papers.', NULL),
+(5, 5, 10, 10, 10, 'good', 'Incredibly fascinating.', 'This article will change your whole perspective on FP.', 'Incredible article! Congratulations!', 'This MUST be accepted. It changed my life.', NULL),
+(6, 6, 11, 2, 8, 'bad', 'This is a disaster.', 'This is a disaster!', 'Don\'t waste our time, next time!', 'Just ignore this crap. He submitted 3 version and they all suck.', NULL),
+(7, 7, 10, 5, 4, 'mediocre', 'Well-organized.', '', 'Good organization.', 'I dont know enough about the subject to really judge this.', NULL),
+(8, 7, 11, 7, 4, 'good', 'Well-organized', '', 'Good organization', 'I dont know enough about the subject to really judge this.', NULL),
+(9, 8, 12, 2, 6, 'bad', 'Just no.', 'Ugh!', 'Why did you write about this?', 'Who cares about this...', NULL),
+(10, 9, 13, 8, 7, 'good', 'Bold subject.', 'This is a bold but truthful look at the situation.', 'You have guts! Congratulations!', 'I think we would do a disservice to our audience to censor this.', NULL);
 
 INSERT INTO `Message` VALUES
 (1, 1, 2, '2014-01-02 00:00:00', b'1', 'Submissions are open!', 'Hi, this is Jerry.  This is just to...', 'Hi, this is Jerry. This is just to let you know that the event is officially open for paper submissions.'),
