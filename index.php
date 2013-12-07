771326aa014da41d53f176021486021b34516173
Philippe Milot <philmilot@gmail.com>
2013-12-07 14:46:26 -0500

    Modifications for ENCS hosting

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 define('SEMS_WEBROOT', __DIR__);
 define('SEMS_UPLOADS', SEMS_WEBROOT.'/uploads');
-include '../dependencies.php';
+include 'dependencies.php';
 
 //Always start the session, this is a very user-driven site
 session_start();
